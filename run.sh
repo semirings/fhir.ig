5e7a1e6c37ef1d455570acf274d878770af9e201
gcr-cormac <geoffry.roberts@gmail.com>
2025-03-27 15:13:04 -0400

    Generates a result

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 DIR=$(pwd)
 cd build/libs || exit 1
-java -jar psoppc.ig-0.0.1.jar -h
-# java -jar psoppc.ig-0.0.1.jar -p StructureDefinition-qicore-adverseevent.xml -i fhir.ecore -o out.ecore
+# java -jar psoppc.ig-0.0.1.jar -h
+java -jar psoppc.ig-0.0.1.jar -p StructureDefinition-qicore-adverseevent.xml -i fhir.ecore -o out.ecore
 cd "$DIR"
